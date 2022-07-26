f4b9c8dfca429eb162b053287bc76de88a85a3bc
Francesco Pischedda <francesco.pischedda@gmail.com>
2022-07-26 22:04:12 +0200

    setup tests for the estimator namespace

--- src/clj/fpsd/estimator.clj
+++ src/clj/fpsd/estimator.clj
@@ -23,24 +23,6 @@
        (reverse)
        (into [])))
 
-(comment
-  (group-by (comp :points second) {"id1" {:points 3 :name "Luke"}
-                "id2" {:points 2 :name "Yaroslav"}
-                "id3" {:points 2 :name "Emmanuel"}
-                "id4" {:points 1 :name "Fra"}
-                "id5" {:points 3 :name "Stanislav"}
-                "id6" {:points 5 :name "Nikhil"}})
-
-  (count-votes {"id1" {:points 3 :name "Luke"}
-                "id2" {:points 2 :name "Yaroslav"}
-                "id3" {:points 2 :name "Emmanuel"}
-                "id4" {:points 1 :name "Fra"}
-                "id5" {:points 3 :name "Stanislav"}
-                "id6" {:points 5 :name "Nikhil"}}) ;; => [{:points 5, :count 1, :authors ["Nikhil"]} {:points 3, :count 2, :authors ["Luke" "Stanislav"]} {:points 2, :count 2, :authors ["Yaroslav" "Emmanuel"]} {:points 1, :count 1, :authors ["Fra"]}]
-
-  (count-votes {"8645c7b2-2049-4346-b75e-196d2ad98ae1" {:points 3, :name "bob", :breakdown {:implementation "", :refactoring "", :tests "", :risk "", :pain ""}}, "b7efd909-a2b4-4d1f-83d1-75fd0ff3190b" {:points 3, :name "alice", :breakdown {:implementation "", :refactoring "", :tests "", :risk "", :pain ""}}}) ;;  => [{:points 3, :count 2, :authors ["bob" "alice"]}]
-  )
-
 (defn votes-delta
   "Returns the difference between the maximum and minumum value in the
   provided sorted seq of vote frequencies; an entry looks like:
@@ -48,15 +30,12 @@
   [frequencies]
   (- (-> frequencies first (:points 0)) (-> frequencies last (:points 0))))
 
-(comment
-  (votes-delta [{:points 5, :count 1, :authors ["Nikhil"]} {:points 3, :count 2, :authors ["Luke" "Stan"]} {:points 2, :count 2, :authors ["Yaroslav" "Emmanuel"]} {:points 1, :count 1, :authors ["Fra"]}] ) ;; => 4
-  ,)
-
 (defn select-winner
   [counted-votes]
   (let [sorted (reverse (sort-by :count counted-votes))
         [{first-vote :points first-voters :authors}
          {second-vote :points second-voters :authors} _] sorted]
+    (println sorted)
     (if (= (count first-voters) (count second-voters))
       {:result :ex-equo
        :suggested (max first-vote second-vote)
@@ -64,12 +43,6 @@
                      {:points second-vote :authors second-voters}]}
       {:result :winner
        :points first-vote})))
-
-(comment
-  (select-winner [{:points 5, :count 1, :authors ["Nikhil"]} {:points 3, :count 2, :authors ["Luke" "Stan"]} {:points 2, :count 2, :authors ["Yaroslav" "Emmanuel"]} {:points 1, :count 1, :authors ["Fra"]}]) ;; => {:result :ex-equo, :suggested 3, :same-votes [2 3]}
-
-  (select-winner [{:points 5, :count 1, :authors ["Nikhil"]} {:points 3, :count 2, :authors ["Luke" "Stan"]} {:points 2, :count 3, :authors ["Yaroslav" "Emmanuel" "Julio"]} {:points 1, :count 1, :authors ["Fra"]}]) ;; => {:result :winner, :points 2}
-  ,)
 
 (defn can-rediscuss
   [sessions settings]
@@ -87,7 +60,9 @@
         result
         (if (or (< delta (:max-points-delta settings))
                 (not (can-rediscuss sessions settings)))
-          (select-winner votes)
+          (do
+            (println votes)
+            (select-winner votes))
 
           (let [{lowest-vote :points lowest-voters :authors} (last votes)
                 {highest-vote :points highest-voters :authors} (first votes)]
@@ -101,40 +76,12 @@
 
 (comment
 
-  (count-votes (-> fpsd.refinements/refinements_ deref (get "HUHGJF") :tickets (get "asdf") :current-session :votes))
-
-  (votes-delta (count-votes (-> fpsd.refinements/refinements_ deref (get "HUHGJF") :tickets (get "asdf") :current-session :votes)))
-
-  (-> fpsd.refinements/refinements_ deref (get "HUHGJF") :tickets (get "asdf") :current-session) ;; {:status :open, :result nil, :votes {"eb550ebf-e206-471f-850c-e9e80443a235" {:points 3, :name "bob", :breakdown {:implementation "", :refactoring "", :tests "", :risk "", :pain ""}}, "4196502c-edb5-4c23-8861-4f042d8ecfed" {:points 3, :name "alice", :breakdown {:implementation "", :refactoring "", :tests "", :risk "", :pain ""}}}, :skips #{}}
-  (estimate (-> fpsd.refinements/refinements_ deref (get "HUHGJF") :tickets (get "asdf")) settings) ;; {:result :winner, :points 3, :votes [{:points 3, :count 2, :authors ["bob" "alice"]}]}
-
-  (estimate ticket settings) ;; {:result :winner, :points 2}
-
   (estimate {:current-session {:story-points nil
-                               :points {"1" {:points 3 :name "Bob"}
-                                        "2" {:points 2 :name "Alice"}
-                                        "3" {:points 2 :name "Joe"}
-                                        "4" {:points 3 :name "Foo"}}}
-             :sessions []}
-            settings) ;; => {:result :ex-equo, :suggested 3, :same-points [[2 ["Alice" "Joe"]] [3 ["Bob" "Foo"]]], :votes [{:points 3, :count 2, :authors ["Bob" "Foo"]} {:points 2, :count 2, :authors ["Alice" "Joe"]}]}
-
-  (estimate {:current-session {:story-points nil
-                               :points {"1" {:points 0 :name "Bob"}
+                               :votes {"1" {:points 0 :name "Bob"}
                                         "2" {:points 1 :name "Alice"}
                                         "3" {:points 4 :name "Joe"}
                                         "4" {:points 3 :name "Foo"}}}
              :sessions []}
             settings) ;; {:result :discuss, :highest-vote 4, :highest-voters ["Joe"], :lowest-vote 0, :lowest-voters ["Bob"], :votes [{:points 4, :count 1, :authors ["Joe"]} {:points 3, :count 1, :authors ["Foo"]} {:points 1, :count 1, :authors ["Alice"]} {:points 0, :count 1, :authors ["Bob"]}]}
 
-  (estimate {:current-session {:story-points nil
-                               :points {"1" {:points 3 :name "Bob"}
-                                        "2" {:points 2 :name "Alice"}
-                                        "3" {:points 1 :name "Joe"}
-                                        "4" {:points 3 :name "Foo"}}}
-             :sessions [{:story-points nil
-                         :points {"1" {:points 4 :name "Bob"}
-                                  "2" {:points 2 :name "Alice"}
-                                  "3" {:points 2 :name "Joe"}
-                                  "4" {:points 0 :name "Foo"}}}]}
-            settings) ;; => {:result :winner, :points 3, :votes [{:points 3, :count 2, :authors ["Bob" "Foo"]} {:points 2, :count 1, :authors ["Alice"]} {:points 1, :count 1, :authors ["Joe"]}]}
   )
--- test/fpsd/estimator_test.clj
+++ test/fpsd/estimator_test.clj
@@ -1,20 +1,78 @@
 (ns fpsd.estimator-test
     (:require
      [clojure.test :refer [deftest is testing]]
-     [fpsd.estimator :refer [multiply]]))
+     [fpsd.estimator :as estimator]))
 
-(testing "Test"
-  (is (= 1 1)))
-#_(testing "Estimator"
-  (testing "Count votes"
-    (is (= (count-votes {"id1" {:points 3 :name "Bob"}
-                         "id2" {:points 2 :name "Alice"}
-                         "id3" {:points 2 :name "Mario"}
-                         "id4" {:points 1 :name "Frank"}
-                         "id5" {:points 3 :name "Foo"}
-                         "id6" {:points 5 :name "Bar"}})
+(testing "Estimator"
+  (deftest count-votes
+    (is (= (estimator/count-votes {"id1" {:points 3 :name "Bob"}
+                                   "id2" {:points 2 :name "Alice"}
+                                   "id3" {:points 2 :name "Mario"}
+                                   "id4" {:points 1 :name "Frank"}
+                                   "id5" {:points 3 :name "Foo"}
+                                   "id6" {:points 5 :name "Bar"}})
 
            [{:points 5, :count 1, :authors ["Bar"]}
             {:points 3, :count 2, :authors ["Bob" "Foo"]}
             {:points 2, :count 2, :authors ["Alice" "Mario"]}
-            {:points 1, :count 1, :authors ["Frank"]}]))))
+            {:points 1, :count 1, :authors ["Frank"]}])))
+
+  (deftest votes-delta
+    (is (= 4 (estimator/votes-delta
+              [{:points 5, :count 1, :authors ["Bar"]}
+               {:points 3, :count 2, :authors ["Bob" "Foo"]}
+               {:points 2, :count 2, :authors ["Alice" "Mario"]}
+               {:points 1, :count 1, :authors ["Frank"]}] )))))
+
+(testing "Select winner"
+  (deftest winner-case
+    (is (= {:result :winner :points 3}
+           (estimator/select-winner
+            [{:points 3, :count 3, :authors ["Bob" "Foo" "Bar"]}
+             {:points 2, :count 2, :authors ["Alice" "Mario"]}
+             {:points 1, :count 1, :authors ["Frank"]}]))))
+  (deftest ex-æquo-case
+    (is (= {:result :ex-equo
+            :suggested 3
+            :same-points [{:authors ["Alice" "Mario"] :points 2}
+                          {:authors ["Bob" "Foo"] :points 3}]}
+           (estimator/select-winner
+            [{:points 3, :count 2, :authors ["Bob" "Foo"]}
+             {:points 2, :count 2, :authors ["Alice" "Mario"]}
+             {:points 1, :count 1, :authors ["Frank"]}])))))
+
+(testing "Estimating a ticket"
+  (deftest winner-case
+    (is (= {:result :winner
+            :points 3
+            :votes [{:authors ["Bob" "Foo" "Bar"], :count 3, :points 3}
+                    {:authors ["Alice" "Mario"], :count 2, :points 2}
+                    {:authors ["Frank"], :count 1, :points 1}]}
+           (estimator/estimate
+            {:current-session
+             {:votes
+              {"id1" {:points 3 :name "Bob"}
+               "id2" {:points 2 :name "Alice"}
+               "id3" {:points 2 :name "Mario"}
+               "id4" {:points 1 :name "Frank"}
+               "id5" {:points 3 :name "Foo"}
+               "id6" {:points 3 :name "Bar"}}}
+             :sessions []}
+            {:max-rediscussions 1
+             :max-points-delta 3}))))
+
+  (deftest ex-æquo-case
+    (is (= {:result :ex-equo
+            :suggested 3
+            :same-points [{:authors ["Alice" "Joe"], :points 2}
+                          {:authors ["Bob" "Foo"], :points 3}]
+            :votes [{:points 3 :count 2 :authors ["Bob" "Foo"]}
+                    {:points 2 :count 2 :authors ["Alice" "Joe"]}]}
+           (estimator/estimate
+            {:current-session {:votes {"1" {:points 3 :name "Bob"}
+                                       "2" {:points 2 :name "Alice"}
+                                       "3" {:points 2 :name "Joe"}
+                                       "4" {:points 3 :name "Foo"}}}
+             :sessions []}
+            {:max-rediscussions 1
+             :max-points-delta 3})))))
