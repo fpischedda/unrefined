d9e9dfde9259e4a9cfa5d00966c3e37a114ba0b7
Francesco Pischedda <francesco.pischedda@gmail.com>
2022-08-02 12:34:43 +0200

    simplify extract-ticket-id-from-url

--- src/clj/fpsd/refinements/helpers.clj
+++ src/clj/fpsd/refinements/helpers.clj
@@ -6,9 +6,8 @@
   "Return the ticket id extracted from the URL of the ticketing system,
    if the format is not recognized then return nil"
   [url]
-  (reduce (fn [_ url-regex]
-            (some->> url (re-matches url-regex) second reduced))
-          nil url-regexes))
+  (when url
+    (some #(second (re-matches % url)) url-regexes)))
 
 (defn try-parse-int
   "Return the integer value represented by the string int-str
