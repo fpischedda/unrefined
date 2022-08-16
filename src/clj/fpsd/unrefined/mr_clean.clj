c6304db50583741117102d96fcc67dc9da81ad0a
Francesco Pischedda <francesco.pischedda@gmail.com>
2022-08-16 20:57:40 +0200

    also close and remove the manifold sink associated to the refinement

--- src/clj/fpsd/unrefined/mr_clean.clj
+++ src/clj/fpsd/unrefined/mr_clean.clj
@@ -1,15 +1,22 @@
 (ns fpsd.unrefined.mr-clean
-  (:require [fpsd.refinements.helpers :refer [utc-now]]
+  (:require [manifold.stream :as s]
+            [fpsd.refinements.helpers :refer [utc-now]]
             [fpsd.unrefined.state :as state]))
 
-(defn filter-expired-refinements
-  [refinements oldest-time]
-  (into {}
-        (filter (fn [[_ refinement]] (.isAfter (:updated-at refinement) oldest-time))
-                refinements)))
+(defn clean-expired-refinements
+  [state oldest-time]
+  (reduce (fn [acc [ref-code refinement]]
+            (if (.isAfter (:updated-at refinement) oldest-time)
+              acc
+              (do
+                (s/close! (-> acc :refinements-sink (get ref-code)))
+                (-> acc
+                    (update :refinements dissoc ref-code)
+                    (update :refinements-sink dissoc ref-code)))))
+          state (:refinements state)))
 
 (defn remove-old-refinements!
   [ttl]
   (state/transact!
    (fn [state]
-     (update state :refinements filter-expired-refinements (.minusSeconds (utc-now) ttl)))))
+     (clean-expired-refinements state (.minusSeconds (utc-now) ttl)))))
--- test/fpsd/unrefined/mr_clean_test.clj
+++ test/fpsd/unrefined/mr_clean_test.clj
@@ -1,5 +1,6 @@
 (ns fpsd.unrefined.mr-clean-test
   (:require [clojure.test :refer [deftest is testing]]
+            [manifold.stream :as s]
             [fpsd.refinements :as refinements]
             [fpsd.refinements.helpers :refer [utc-now]]
             [fpsd.unrefined.mr-clean :as mr-clean]
@@ -7,24 +8,30 @@
 
 (testing "Remove refinements past ttl"
   (deftest filter-expired-refinements
-    (let [refinements {"REF1" (refinements/create "REF1")}
+    (let [state {:refinements {"REF1" (refinements/create "REF1")}
+                 :refinements-sink {"REF1" (s/stream)}}
           oldest-time (.plusSeconds (utc-now) 70)]
-      (is (= {}
-             (mr-clean/filter-expired-refinements refinements oldest-time)))))
+      (is (= {:refinements {}
+              :refinements-sink {}}
+             (mr-clean/clean-expired-refinements state oldest-time)))))
 
   (deftest keep-non-expired-refinements
-    (let [refinements {"REF1" (refinements/create "REF1")}
+    (let [state {:refinements {"REF1" (refinements/create "REF1")}
+                 :refinements-sink {"REF1" "fake-stream"}}
           oldest-time (.minusSeconds (utc-now) 50)]
-      (is (= refinements
-             (mr-clean/filter-expired-refinements refinements oldest-time))))))
+      (is (= state
+             (mr-clean/clean-expired-refinements state oldest-time))))))
 
 (testing "Update the state removing refinements past ttl"
   (deftest statefu-filter-expired-refinements
-    (let [_ (state/transact! (fn [state] (assoc state :refinements {"REF1" (refinements/create "REF1")})))
+    (let [_ (state/transact! (fn [state] {:refinements {"REF1" (refinements/create "REF1")}
+                                          :refinements-sink {"REF1" (s/stream)}}))
           _ (mr-clean/remove-old-refinements! -50)]
-      (is (nil? (state/get-refinement "REF1")))))
+      (is (nil? (or (state/get-refinement "REF1") (state/get-refinement-sink "REF1"))))))
 
   (deftest stateful-keep-non-expired-refinements
-    (let [_ (state/transact! (fn [state] (assoc state :refinements {"REF1" (refinements/create "REF1")})))
+    (let [_ (state/transact! (fn [state] {:refinements {"REF1" (refinements/create "REF1")}
+                                          :refinements-sink {"REF1" "fake-stream"}}))
           _ (mr-clean/remove-old-refinements! 50)]
-      (is (some? (state/get-refinement "REF1"))))))
+      (is (and (some? (state/get-refinement "REF1"))
+               (some? (state/get-refinement-sink "REF1")))))))
