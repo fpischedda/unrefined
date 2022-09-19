c1b5f5f6e0af697ada2fc44d8ec71e95e89a9154
Francesco Pischedda <francesco.pischedda@gmail.com>
2022-09-19 15:32:30 +0200

    started restructuring routes + adding coercion with malli

--- src/clj/fpsd/routes.clj
+++ src/clj/fpsd/routes.clj
@@ -1,6 +1,9 @@
 (ns fpsd.routes
   (:require [reitit.ring :as ring]
             [reitit.ring.middleware.muuntaja :as muuntaja]
+            [reitit.ring.coercion :as rrc]
+            [reitit.coercion.malli]
+            [reitit.core :as r]
             [muuntaja.core :as m]
             [aleph.http :as http]
             [mount.core :as mount]
@@ -33,37 +36,48 @@
       (tap> response)
       response)))
 
-(def handler
-  (ring/ring-handler
-   (ring/router
-    [["/" {:get handlers/index}]
-     ["/assets/*" (ring/create-resource-handler)]
+(def app-router
+  (ring/router
+   [["/" {:get handlers/index}]
+    ["/assets/*" (ring/create-resource-handler)]
 
-     ["/refine"
-      ["" {:get handlers/index
-           :post handlers/create-refinement}]
+    ["/refine"
+     ["" {:get handlers/index
+          :post {:handler handlers/create-refinement
+                 ;; :coercion reitit.coercion.malli/coercion
+                 :parameters {:body {:ticket-url :string}}}}]
 
-      ["/:code/ticket/:ticket-id" {:get handlers/estimate-watch}]
-      ["/:code/ticket/:ticket-id/results" {:get handlers/estimate-results}]
+     ["/:code" {:coercion reitit.coercion.malli/coercion
+                :parameters {:path {:code :string}}}
 
-      ["/:code/ticket/:ticket-id/estimate" {:get handlers/estimate-view
-                                            :post handlers/estimate-done}]
-      ["/:code/ticket/:ticket-id/re-estimate" {:post handlers/estimate-again}]
-      ["/:code/ticket/:ticket-id/events" {:get handlers/events-stream-handler}]
-      ["/:code/ticket" {:post handlers/add-ticket}]
-      ["/:code/events" {:get handlers/events-stream-handler}]]]
+      ["/ticket"
+       ["" {:post {:handler handlers/add-ticket
+                   :parameters {:body {:ticket-id :string}}}}]
 
-    {:data {:muuntaja m/instance
-            :middleware [wrap-cookies
-                         wrap-session
-                         wrap-params
-                         wrap-keyword-params
-                         common-cookies
-                         ;; tap-request-response
-                         muuntaja/format-middleware]}})))
+       ["/:ticket-id" {:parameters {:path {:ticket-id :string}}}
+        ["" {:get {:handler handlers/estimate-watch}}]
+        ["/results" {:get handlers/estimate-results}]
+        ["/estimate" {:get handlers/estimate-view
+                      :post handlers/estimate-done}]
+        ["/re-estimate" {:post handlers/estimate-again}]
+        ["/events" {:get handlers/events-stream-handler}]]]
+
+      ["events" {:get handlers/events-stream-handler}]]]]
+
+   {:data {:muuntaja m/instance
+           :middleware [wrap-cookies
+                        wrap-session
+                        wrap-params
+                        wrap-keyword-params
+                        common-cookies
+                        ;; tap-request-response
+                        rrc/coerce-exceptions-middleware
+                        rrc/coerce-request-middleware
+                        rrc/coerce-response-middleware
+                        muuntaja/format-middleware]}}))
 
 (mount/defstate http-server
-  :start (http/start-server #'handler {:port (-> config :http :port)})
+  :start (http/start-server (ring/ring-handler app-router) {:port (-> config :http :port)})
   :stop (.close http-server))
 
 (comment
