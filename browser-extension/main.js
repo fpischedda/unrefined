26fe474882880c4116b5485c259dac6ba674f2b7
Francesco Pischedda <francesco.pischedda@gmail.com>
2023-02-19 13:31:10 +0100

    copy estimation link to clipboard, button to goto watch

--- browser-extension/main.js
+++ browser-extension/main.js
@@ -1,37 +1,46 @@
 function refinementStarted(data) {
-   let activityElement = document.getElementById('activity')
-   const refinementURL = 'http://localhost:8080' + data['refinement-path']
-   const estimationURL = refinementURL + '/estimate'
+    let activityElement = document.getElementById('activity')
+    const refinementURL = 'http://localhost:8080' + data['refinement-path']
+    const estimationURL = refinementURL + '/estimate'
 
-   navigator.clipboard.writeText(estimationURL)
-   activityElement.innerHTML = `Estimation link copied to clipboard!<br/>Click <a href="${refinementURL}">here</a> for live updates.`
+    navigator.clipboard.writeText(estimationURL)
+
+    const text = document.createTextNode('Estimation link copied to clipboard!')
+    activityElement.appendChild (text)
+
+    let button = document.createElement ('button')
+    button.appendChild (document.createTextNode ('Click here for live updates'))
+    button.addEventListener ('click', () => {
+	chrome.tabs.create ({'url': refinementURL})
+    })
+    activityElement.appendChild (button)
 }
 
 function getTicketURLFromCurrentTab(callback) {
-   chrome.tabs.query({active: true, lastFocusedWindow: true}, (tabs) => {
-      callback(tabs[0].url) })
+    chrome.tabs.query({active: true, lastFocusedWindow: true}, (tabs) => {
+	callback(tabs[0].url) })
 }
 
 function refineTicket(ticketURL) {
-   console.log("ticket url: " + ticketURL) 
+    console.log("ticket url: " + ticketURL) 
 
-   fetch('http://127.0.0.1:8080/api/refine', {
-      method: 'POST',
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify({'ticket-url': ticketURL})
-   }).then((response) => {
-      console.log(response)
-      return response.json()
-   }).then((data) => {
-      console.log(data)
-      refinementStarted(data)
-   })
+    fetch('http://127.0.0.1:8080/api/refine', {
+	method: 'POST',
+	headers: {'Content-Type': 'application/json'},
+	body: JSON.stringify({'ticket-url': ticketURL})
+    }).then((response) => {
+	console.log(response)
+	return response.json()
+    }).then((data) => {
+	console.log(data)
+	refinementStarted(data)
+    })
 }
 
 function setup() {
-   let startButton = document.getElementById('startRefinementButton')
+    let startButton = document.getElementById('startRefinementButton')
 
-   startButton.addEventListener( 'click', () => { getTicketURLFromCurrentTab(refineTicket) })
+    startButton.addEventListener( 'click', () => { getTicketURLFromCurrentTab(refineTicket) })
 }
 
 setup()
